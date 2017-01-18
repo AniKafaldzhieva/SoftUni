c608061e997d319251ebc7849ba5b4e5a0f1184e
AniKafaldzhieva <anikafaldzhieva@abv.bg>
2017-01-18 18:41:38 +0200

    Circle area

---
+++ 1. C# Fundamentals/DataTypesExercises/CircleArea(Precision 12)/CircleArea1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class CircleArea1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Circle area is: {0:f12}", Math.PI * r * r);
+        }
+    }
+}
