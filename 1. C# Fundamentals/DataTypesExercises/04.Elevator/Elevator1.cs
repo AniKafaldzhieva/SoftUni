429d160ad25d3bfc3de5d271123ba0a20bf3b096
AniKafaldzhieva <anikafaldzhieva@abv.bg>
2017-01-18 19:06:37 +0200

    Elevator

---
+++ 1. C# Fundamentals/DataTypesExercises/04.Elevator/Elevator1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Elevator1
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int elevator = int.Parse(Console.ReadLine());
+
+            if(people>elevator)
+            {
+                int courses = (int)Math.Ceiling((double)people / elevator);
+                Console.WriteLine(courses);
+            }
+            else
+            {
+                int courses = (int)Math.Ceiling((double)elevator % people);
+                Console.WriteLine(courses);
+            }
+        }
+    }
+}
